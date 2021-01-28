52805ab4448747374feb6cb780bd8b5581f850cd
kemokaba <kemokaba@gmail.com>
2021-01-28 04:39:46 +0100

    my first commit

---
+++ blog/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from blog.models import Comment
+
+class CreateCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['author_name','text']
